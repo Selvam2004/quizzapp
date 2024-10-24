a73ccb265893b6ed4356cbeb4120a34898d78f5d
Selvam2004 <selvam032004@gmail.com>
2024-10-24 18:26:17 +0530

    update result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,5 @@
-const Results = ({ score, total, results, restartQuiz }) => {
-    const percentage = ((score / total) * 100).toFixed(2);
+const Results = ({ correct,incorrect,skip,total}) => {
+    const percentage = ((correct / total) * 100).toFixed(2);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -9,29 +9,32 @@ const Results = ({ score, total, results, restartQuiz }) => {
                     <tbody>
                         <tr className="border-b">
                             <td className="py-2">Correct:</td>
-                            <td className="py-2 text-right">10 /15 {total}</td>
+                            <td className="py-2 text-right"> {correct}/{total}</td>
                         </tr>
                         <tr className="border-b">
                             <td className="py-2">Incorrect:</td>
-                            <td className="py-2 text-right">2/15</td>
+                            <td className="py-2 text-right">{incorrect}/{total}</td>
                         </tr>
                         <tr className="border-b">
                             <td className="py-2">Skipped:</td>
-                            <td className="py-2 text-right">3/15</td>
+                            <td className="py-2 text-right">{skip}/{skip}</td>
                         </tr>
                         <tr>
                             <td className="py-2 font-bold">Total Score:</td>
-                            <td className="py-2 text-right font-bold">{score} 10/15 {total}</td>
+                            <td className="py-2 text-right font-bold">{correct}/{total}</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+            <a href="/">
             <button 
-                onClick={restartQuiz} 
                 className="mt-4 px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-600"
             >
                 Restart Quiz
             </button>
+            </a>
+ 
+
         </div>
     );
 };
